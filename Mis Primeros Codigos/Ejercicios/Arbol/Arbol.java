265bcca458ed2a9ba772436e3e5786d041a9aa99
Cristopher-Zuluaga <142941619+Cristopher-Zuluaga@users.noreply.github.com>
2023-08-22 17:41:03 -0500

    Update Arbol.java

--- Mis Primeros Codigos/Ejercicios/Arbol/Arbol.java
+++ Mis Primeros Codigos/Ejercicios/Arbol/Arbol.java
@@ -1,8 +1,5 @@
-
-package arbol;
-
-public class Arbol {
-
+public class Arbol 
+    {
     public static void main(String[] args)
     {
         System.out.println("¡Hola! Soy Cristopher Zuluaga y ahora veras un arbol hecho por mi en java");
@@ -15,6 +12,5 @@ public class Arbol {
         System.out.println("    *    ");
         System.out.println("    *    ");
         System.out.println("    *    ");
-    }
-    
+    } 
 }
