b28995418d5aa9eb58ac0ea910702d527d90b3b0
Cristopher-Zuluaga <142941619+Cristopher-Zuluaga@users.noreply.github.com>
2023-08-22 17:46:13 -0500

    Add files via upload

---
+++ Mis Primeros Codigos/Ejercicios/Condicionales/CondicionalEjemplo1.java
@@ -0,0 +1,14 @@
+
+package condicional.ejemplo.pkg1;
+public class CondicionalEjemplo1 
+{
+    public static void main(String[] args) 
+    {
+        int x=20;
+        int y=15;
+        if(x+y > 20)
+        {
+            System.out.println("x + y es mayor que 20");
+        }
+    } 
+}
