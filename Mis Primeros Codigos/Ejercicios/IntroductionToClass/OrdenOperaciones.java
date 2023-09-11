5efc3d14eba1d20fb5382123b68428170e1c6dae
Cristopher-Zuluaga <142941619+Cristopher-Zuluaga@users.noreply.github.com>
2023-09-11 11:34:54 -0500

    Create OrdenOperaciones.java

---
+++ Mis Primeros Codigos/Ejercicios/IntroductionToClass/OrdenOperaciones.java
@@ -0,0 +1,11 @@
+public class OrdenOperaciones {
+  public static void main(String[] args) 
+  {
+    int expresion1 = 5 % 2 - (4 * 2 - 1);
+    System.out.println(expresion1);
+    int expresion2 = (3 +(2 * 2 - 5)) + 6 - 5;
+    System.out.println(expresion2);
+    int expresion3 = 5 * 4 % 3 - 2 + 1;
+    System.out.println(expresion3);
+  }
+}
