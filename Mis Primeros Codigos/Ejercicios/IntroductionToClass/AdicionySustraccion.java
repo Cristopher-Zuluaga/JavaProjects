24e65b55c3eaedc9e9588406474d411051efaadc
Cristopher-Zuluaga <142941619+Cristopher-Zuluaga@users.noreply.github.com>
2023-09-11 11:23:30 -0500

    Create AdicionySustraccion.java

---
+++ Mis Primeros Codigos/Ejercicios/IntroductionToClass/AdicionySustraccion.java
@@ -0,0 +1,11 @@
+public class AdicionySustraccion {
+  public static void main(String[] args) 
+  {
+    int zebrasInZoo = 8;
+    int giraffesInZoo = 4;
+    int animalsInZoo = zebrasInZoo + giraffesInZoo;
+    System.out.println(animalsInZoo);
+    int numzebrasAfterTrade = zebrasInZoo - 2;
+    System.out.println(numzebrasAfterTrade);
+  }
+}
