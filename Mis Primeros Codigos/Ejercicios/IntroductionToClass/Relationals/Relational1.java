262aacff0bf35e60f3040527ae95393c9b3c80fc
Cristopher-Zuluaga <142941619+Cristopher-Zuluaga@users.noreply.github.com>
2023-09-11 11:40:48 -0500

    Create Relational1.java

---
+++ Mis Primeros Codigos/Ejercicios/IntroductionToClass/Relationals/Relational1.java
@@ -0,0 +1,12 @@
+public class Relational1 {
+  public static void main(String[] args) 
+  {
+    double creditsEarned = 176.5;
+    double creditsForSeminar = 8;
+    double creditsToGraduate = 180;
+    double creditsAfterSeminar = creditsEarned + creditsForSeminar;
+    System.out.println(creditsEarned > creditsToGraduate);
+    System.out.println(creditsToGraduate < creditsAfterSeminar);
+    
+  }
+}
