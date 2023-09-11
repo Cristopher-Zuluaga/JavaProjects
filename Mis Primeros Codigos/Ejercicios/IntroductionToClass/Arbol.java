24239a1cc5516baa719d9bca2768418e769bc34d
Cristopher-Zuluaga <142941619+Cristopher-Zuluaga@users.noreply.github.com>
2023-09-11 11:21:38 -0500

    Create Arbol.java

---
+++ Mis Primeros Codigos/Ejercicios/IntroductionToClass/Arbol.java
@@ -0,0 +1,16 @@
+public class Arbol 
+    {
+    public static void main(String[] args)
+    {
+        System.out.println("¡Hola! Soy Cristopher Zuluaga y ahora veras un arbol hecho por mi en java");
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println(" ******* ");
+        System.out.println("  *****  ");
+        System.out.println("   ***   ");
+        System.out.println("    *    ");
+        System.out.println("    *    ");
+        System.out.println("    *    ");
+        System.out.println("    *    ");
+    } 
+}
