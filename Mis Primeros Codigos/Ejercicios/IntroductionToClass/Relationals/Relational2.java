cec7ac0285c12d3d7d9e14b46bcd7d274940e61f
Cristopher-Zuluaga <142941619+Cristopher-Zuluaga@users.noreply.github.com>
2023-09-11 11:41:27 -0500

    Create Relational2.java

---
+++ Mis Primeros Codigos/Ejercicios/IntroductionToClass/Relationals/Relational2.java
@@ -0,0 +1,13 @@
+public class Relational2 {
+  public static void main(String[] args) 
+  {
+    int songsA = 9;
+    int songsB = 9;
+    int AlbumLengthA = 41;
+    int AlbumLengthB = 53;
+    boolean sameNumberOfSong = songsA == songsB;
+    boolean differentLength = AlbumLengthA != AlbumLengthB;
+    System.out.println(sameNumberOfSong);
+    System.out.println(differentLength);
+  }
+}
