ac511317045bf1824e95ac305c3c252ffed14870
Cristopher-Zuluaga <142941619+Cristopher-Zuluaga@users.noreply.github.com>
2023-09-12 16:33:50 -0500

    Create ChekinAccount.java

---
+++ Mis Primeros Codigos/Ejercicios/Encapsulacion/Bank/ChekinAccount.java
@@ -0,0 +1,10 @@
+public class ChekingAccount 
+{
+	public String name;
+	public int balance;
+	public ChekingAccount(String inputname, int inputbalance)
+	{
+		name = inputname;
+		balance = inputbalance;
+	}
+}
