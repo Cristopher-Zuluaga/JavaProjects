1414fca26ed2555272267eff461de1c5d363ba9f
Cristopher-Zuluaga <142941619+Cristopher-Zuluaga@users.noreply.github.com>
2023-09-12 16:12:42 -0500

    Create BreakExample4.java

---
+++ Mis Primeros Codigos/Ejercicios/Loops/Break/BreakExample4.java
@@ -0,0 +1,14 @@
+public class BreakExample4 {  
+public static void main(String[] args) {  
+    //while loop  
+    int i=1;  
+    while(i<=10){  
+        if(i==5){   
+            i++;  
+            break;
+        }  
+        System.out.println(i);  
+        i++;  
+    }  
+}  
+}  
